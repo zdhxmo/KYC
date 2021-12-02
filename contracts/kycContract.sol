b99839f634f56f8f1562ea5c8cfce72b76a41753
dhruvMohapatra <dhrvmohapatra@gmail.com>
2021-12-02 17:24:22 +0530

    feat: Add Customer function

--- contracts/kycContract.sol
+++ contracts/kycContract.sol
@@ -4,9 +4,7 @@ contract kycContract {
     struct Customer {
         string username;
         // hash of the documents submitted by the customer
-        string dataHash;
-        // upvotes by other banks
-        uint256 upVotes;
+        string customerData;
         // unique address of the validating bank
         address validatorBankAddress;
     }
@@ -18,5 +16,35 @@ contract kycContract {
         // registration number of the bank
         string regNumber;
     }
+
+    mapping(string => Customer) customers;
+    mapping(address => Bank) banks;
+
+    // function to view customer
+    function viewCustomer(string memory customerName)
+        public
+        returns (string memory)
+    {}
+
+    // function to add customers mapped to their documents
+    function addCustomer(
+        string memory _customerName,
+        string memory _customerData
+    ) public {
+        require(
+            customers[_customerName].validatorBankAddress == address(0),
+            "User exists. Please call modifyCustomer to make any changes"
+        );
+
+        customers[_customerName].username = _customerName;
+        customers[_customerName].customerData = _customerData;
+        customers[_customerName].validatorBankAddress = msg.sender;
+    }
+
+    // function to edit customer details
+    function modifyCustomer(
+        string memory customerName,
+        string memory newDataHash
+    ) public returns (uint256) {}
 
 }
