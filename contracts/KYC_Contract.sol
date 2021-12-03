94993ab8e79f0ba2ea902fe6a57be12d438daddb
dhruvMohapatra <dhrvmohapatra@gmail.com>
2021-12-03 22:27:41 +0530

    feat: Add upvote and downVote customer functions

--- contracts/KYC_Contract.sol
+++ contracts/KYC_Contract.sol
@@ -24,9 +24,9 @@ contract KYC_Contract {
         // status of KYC request. if conditions are met, set to true. else false
         bool kycRequest;
         // number of upvotes from other banks to customer data
-        uint256 upVotes;
+        uint32 upVotes;
         // number of downvotes from other banks to customer data
-        uint256 downVotes;
+        uint32 downVotes;
         // unique address of the validating bank
         address validatorBankAddress;
     }
@@ -36,9 +36,9 @@ contract KYC_Contract {
         // unique ethereum address of the bank
         address ethAddress;
         // total complaints against the bank by other banks in the network
-        uint256 complaintsReported;
+        uint32 complaintsReported;
         //number of KYC requests initiated by bank
-        uint256 KYC_count;
+        uint32 KYC_count;
         // status. if false, bank cannot up/down vote any more customers
         bool isAllowedToVote;
         // registration number of the bank
@@ -46,7 +46,7 @@ contract KYC_Contract {
     }
 
     // count of total banks in the network
-    uint256 totalBanks;
+    uint32 totalBanks;
 
     struct KYC_Request {
         // map KYC request to customer data
@@ -68,19 +68,27 @@ contract KYC_Contract {
     // customer name to view KYC requests
     mapping(string => KYC_Request) KYC_requests;
 
+    // keep track of up votes for the customer by all banks in the network
+    mapping(string => mapping(address => uint8)) up_votes;
+
+    // keep track of down votes for the customer by all banks in the network
+    mapping(string => mapping(address => uint8)) down_votes;
+
     /* Events */
 
     event AddKYCRequest(string _customerName, string _customerData);
     event RemoveKYCRequest(string _customerName);
     event AddCustomer(string _customerName, string _customerData);
     event ModifyCustomer(string _customerName, string _newCustomerData);
+    event UpVoteCustomer(string _customerName);
+    event DownVoteCustomer(string _customerName);
 
-    /*
+    /**
      * Record a new KYC request on behalf of a customer
      * Bank is the sender of this request
-     * @param {string}   _customerName The name of the customer for whom KYC is to be done
-     * @param {string}  _customerData The hash of the customer data being requested
-     * returns {bool}  returns true
+     * @param _customerName The name of the customer for whom KYC is to be done
+     * @param _customerData The hash of the customer data being requested
+     * @return bool
      */
     function addKYCRequest(
         string memory _customerName,
@@ -104,11 +112,10 @@ contract KYC_Contract {
         return true;
     }
 
-    /*
+    /**
      * Remove KYC request
-     * @param {string} _customerName The name of the customer for whom KYC is to be removed
-     * @param  {string} _customerData Hash of the customer's data
-     * returns {bool}  returns true
+     * @param _customerName The name of the customer for whom KYC is to be removed
+     * @return bool
      */
     function removeKYCRequest(string memory _customerName)
         public
@@ -130,10 +137,10 @@ contract KYC_Contract {
         return true;
     }
 
-    /*
+    /**
      * View customer information
-     * @param  {public}  _customerName Name of the customer
-     * @return {Customer} The customer struct as an object
+     * @param _customerName Name of the customer
+     * @return struct Customer - The customer struct as an object
      */
     function viewCustomer(string memory _customerName)
         public
@@ -156,11 +163,11 @@ contract KYC_Contract {
         );
     }
 
-    /*
+    /**
      * Add a new customer
-     * @param {string} _userName Name of the customer
-     * @param {string} _customerData Hash of the customer docs
-     * returns {bool}  returns true
+     * @param _customerName Name of the customer
+     * @param _customerData Hash of the customer docs
+     * @return bool
      */
     function addCustomer(
         string memory _customerName,
@@ -179,10 +186,11 @@ contract KYC_Contract {
         return true;
     }
 
-    /*
+    /**
      * Modify customer data
-     * @param  {string} _customerName Name of the customer
-     * @param  {string} _newCustomerData New hash of the updated docs
+     * @param _customerName Name of the customer
+     * @param _newCustomerData New hash of the updated docs
+     * @return bool
      */
     function modifyCustomer(
         string memory _customerName,
@@ -197,5 +205,72 @@ contract KYC_Contract {
 
         emit ModifyCustomer(_customerName, _newCustomerData);
         return true;
+    }
+
+    /**
+     * Function to add a new up vote for a customer
+     * @param _customerName Name of the customer to be upvoted
+     * @return bool
+     */
+    function upVoteCustomer(string memory _customerName) public returns (bool) {
+        require(
+            compareStringsbyBytes(
+                _customerName,
+                customers[_customerName].username
+            ),
+            "Customer doesn't exist. Use addCustomer to create new user"
+        );
+
+        require(
+            up_votes[_customerName][msg.sender] != 0,
+            "Customer has already been upvoted by you."
+        );
+
+        // update upVote count
+        customers[_customerName].upVotes++;
+        up_votes[_customerName][msg.sender] = 1;
+
+        emit UpVoteCustomer(_customerName);
+        return true;
+    }
+
+    /**
+     * Function to add a new down vote for a customer
+     * @param _customerName Name of the customer to be upvoted
+     * @return bool
+     */
+    function downVoteCustomer(string memory _customerName)
+        public
+        returns (bool)
+    {
+        require(
+            compareStringsbyBytes(
+                _customerName,
+                customers[_customerName].username
+            ),
+            "Customer doesn't exist. Use addCustomer to create new user"
+        );
+
+        require(
+            down_votes[_customerName][msg.sender] != 0,
+            "Customer has already been down voted by you."
+        );
+
+        // update upVote count
+        customers[_customerName].downVotes++;
+        down_votes[_customerName][msg.sender] = 1;
+
+        emit DownVoteCustomer(_customerName);
+        return true;
+    }
+
+    // source: https://ethereum.stackexchange.com/questions/45813/compare-strings-in-solidity
+    function compareStringsbyBytes(string memory s1, string memory s2)
+        public
+        pure
+        returns (bool)
+    {
+        return
+            keccak256(abi.encodePacked(s1)) == keccak256(abi.encodePacked(s2));
     }
 }
