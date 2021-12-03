ef5f49e762d136ae632faef5808c9bd6f1497550
dhruvMohapatra <dhrvmohapatra@gmail.com>
2021-12-03 21:40:00 +0530

    feat: Remove KYC request and emit events

--- contracts/KYC_Contract.sol
+++ contracts/KYC_Contract.sol
@@ -3,6 +3,20 @@
 pragma solidity ^0.8.9;
 
 contract KYC_Contract {
+    constructor() {
+        admin = msg.sender;
+    }
+
+    address admin;
+
+    modifier onlyAdmin() {
+        require(
+            msg.sender == admin,
+            "You must have admin priviledges to run this"
+        );
+        _;
+    }
+
     struct Customer {
         string username;
         // hash that points to customer documents in secure storage
@@ -34,7 +48,6 @@ contract KYC_Contract {
     // count of total banks in the network
     uint256 totalBanks;
 
-
     struct KYC_Request {
         // map KYC request to customer data
         string username;
@@ -44,38 +57,77 @@ contract KYC_Contract {
         address bankAddress;
     }
 
+    /* Mappings */
+
     // customer name to view customer details
     mapping(string => Customer) customers;
 
     // unique bank address to view bank details
     mapping(address => Bank) banks;
 
-    // customer document hash to view kyc requests
-    mapping(string => KYC_Request) KYCrequests;
+    // customer name to view KYC requests
+    mapping(string => KYC_Request) KYC_requests;
+
+    /* Events */
+
+    event AddKYCRequest(string _customerName, string _customerData);
+    event RemoveKYCRequest(string _customerName);
+    event AddCustomer(string _customerName, string _customerData);
+    event ModifyCustomer(string _customerName, string _newCustomerData);
 
     /*
      * Record a new KYC request on behalf of a customer
      * Bank is the sender of this request
      * @param {string}   _customerName The name of the customer for whom KYC is to be done
      * @param {string}  _customerData The hash of the customer data being requested
+     * returns {bool}  returns true
      */
     function addKYCRequest(
         string memory _customerName,
         string memory _customerData
-    ) public {
+    ) public returns (bool) {
         // require that the request the bank is making isn't a duplicate
         require(
-            !(KYCrequests[_customerName].bankAddress == msg.sender),
+            !(KYC_requests[_customerName].bankAddress == msg.sender),
             "A KYC request already exists for this user from this bank"
         );
 
         // update KYC requests mapping
-        KYCrequests[_customerName].username = _customerName;
-        KYCrequests[_customerName].customerData = _customerData;
-        KYCrequests[_customerName].bankAddress = msg.sender;
+        KYC_requests[_customerName].username = _customerName;
+        KYC_requests[_customerName].customerData = _customerData;
+        KYC_requests[_customerName].bankAddress = msg.sender;
 
         // add KYC requests made by this bank
         banks[msg.sender].KYC_count++;
+
+        emit AddKYCRequest(_customerName, _customerData);
+        return true;
+    }
+
+    /*
+     * Remove KYC request
+     * @param {string} _customerName The name of the customer for whom KYC is to be removed
+     * @param  {string} _customerData Hash of the customer's data
+     * returns {bool}  returns true
+     */
+    function removeKYCRequest(string memory _customerName)
+        public
+        returns (bool)
+    {
+        // require that the username in fact exists as a KYC request
+        require(
+            !(KYC_requests[_customerName].bankAddress == msg.sender),
+            "No KYC request exists for this customer. Please use addKYCRequest function"
+        );
+
+        // delete the customer's KYC request
+        delete KYC_requests[_customerName];
+
+        // reduce KYC requests made by this bank
+        banks[msg.sender].KYC_count--;
+
+        emit RemoveKYCRequest(_customerName);
+        return true;
     }
 
     /*
@@ -108,11 +160,12 @@ contract KYC_Contract {
      * Add a new customer
      * @param {string} _userName Name of the customer
      * @param {string} _customerData Hash of the customer docs
+     * returns {bool}  returns true
      */
     function addCustomer(
         string memory _customerName,
         string memory _customerData
-    ) public {
+    ) public returns (bool) {
         require(
             customers[_customerName].validatorBankAddress == address(0),
             "User exists. Please call modifyCustomer to make any changes"
@@ -121,6 +174,9 @@ contract KYC_Contract {
         customers[_customerName].username = _customerName;
         customers[_customerName].customerData = _customerData;
         customers[_customerName].validatorBankAddress = msg.sender;
+
+        emit AddCustomer(_customerName, _customerData);
+        return true;
     }
 
     /*
@@ -131,12 +187,15 @@ contract KYC_Contract {
     function modifyCustomer(
         string memory _customerName,
         string memory _newCustomerData
-    ) public {
+    ) public returns (bool) {
         require(
             customers[_customerName].validatorBankAddress == address(0),
             "User doesn't exist. Please call addCustomer to create new user"
         );
 
         customers[_customerName].customerData = _newCustomerData;
+
+        emit ModifyCustomer(_customerName, _newCustomerData);
+        return true;
     }
 }
